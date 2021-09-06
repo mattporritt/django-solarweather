69197af992e7f4ddf6665f4ddca9ce44ac3ddc45
Matt Porritt <mattp@catalyst-au.net>
2021-09-06 10:33:08 +0000

    add management command to get data from inverter

---
+++ solar/management/commands/queryinverter.py
@@ -0,0 +1,40 @@
+# ==============================================================================
+#
+# This file is part of SolarWeather.
+#
+# SolarWeather is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SolarWeather is distributed  WITHOUT ANY WARRANTY:
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# ==============================================================================
+
+# ==============================================================================
+#
+# @author Matthew Porritt
+# @copyright  2021 onwards Matthew Porritt (mattp@catalyst-au.net)
+# @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+# ==============================================================================
+
+from django.core.management.base import BaseCommand
+from solar.solardata import SolarData
+
+
+class Command(BaseCommand):
+    help = 'Query the inverter for latest data.'
+
+    def handle(self, *args, **options):
+        """
+        Process the command line getting of inverter data.
+        """
+
+        solar_data = SolarData()
+        solar_data.store()
+
+        self.stdout.write(self.style.SUCCESS('Successfully processed data.'))
